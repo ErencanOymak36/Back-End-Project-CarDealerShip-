22c710744b18e904238063072ed6a5638db5bdc3
ErencanOymak36 <133775010+ErencanOymak36@users.noreply.github.com>
2023-05-16 16:28:44 +0300

    Add files via upload

---
+++ DataAccess/Concrete/EntityFramework/EfCarDal.cs
@@ -0,0 +1,38 @@
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.DTOs;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfCarDal : EfEntitiyRepositoryBase<Car, Context>, ICarDal
+    {
+        public List<CarDetailDto> GetCarDetail()
+        {
+            using (Context db= new Context())
+            {
+                var result = from c in db.Cars
+                             join b in db.Brands
+                             on c.BrandId equals b.BrandId 
+                             from ca in db.Cars
+                             join cl in db.Colors
+                             on ca.ColorId equals cl.ColorId
+                             select new CarDetailDto { BrandName = b.BrandName, ModelYear = c.ModelYear, DailyPrice = c.DailyPrice, ColorName=cl.ColorName };
+                            
+
+                
+              
+                return result.ToList();
+
+            }
+        }
+    }
+}
---
+++ DataAccess/Migrations/20230424123718_UserUpdate.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Password",
+                table: "Users",
+                newName: "PasswordSalt");
+
+            migrationBuilder.AddColumn<int>(
+                name: "PasswordHash",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Status",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PasswordHash",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "PasswordSalt",
+                table: "Users",
+                newName: "Password");
+        }
+    }
+}
