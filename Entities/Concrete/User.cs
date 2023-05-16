18e8ef7b70d6bbedd05266d8ad8ec73f73e2098b
ErencanOymak36 <133775010+ErencanOymak36@users.noreply.github.com>
2023-05-16 16:29:35 +0300

    Add files via upload

---
+++ Entities/Concrete/User.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class User:IEntity
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int PasswordHash { get; set; }
+        public int PasswordSalt { get; set; }
+        public bool Status { get; set; }
+        public virtual ICollection<Customer>? Customer { get; set; }
+
+    }
+}
